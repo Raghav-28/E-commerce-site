c8b4033412d2359ee9b7a5798e8a263d74c8b766
Raghav Gupta <kraghavgupta@gmail.com>
2024-07-05 17:06:51 +0530

    Cart Section linking with info

--- assets/js/addcart.js
+++ assets/js/addcart.js
@@ -1,20 +1,20 @@
 const product = [
     {
-        id: 0,
+        id: 'onemukhi',
         image: 'assets/img/portfolio/onemukhi.webp',
         title: 'Raghav Bhaiya',
         price: 120,
         category: 'beads',
     },
     {
-        id: 1,
+        id: 'twomukhi',
         image: 'assets/img/portfolio/twomukhi.jpg',
         title: 'Raghav Bhaiya',
         price: 130,
         category: 'rudrakshmala',
     },
     {
-        id: 2,
+        id: 'threemukhi',
         image: 'assets/img/portfolio/threemukhi.webp',
         title: 'Raghav Bhaiya',
         price: 180,
@@ -55,8 +55,288 @@ const product = [
         price: 120,
         category: 'rudrakshmala',
     },
-    
+    {
+        id: 8,
+        image: 'assets/img/portfolio/onemukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'beads',
+    },
+    {
+        id: 9,
+        image: 'assets/img/portfolio/twomukhi.jpg',
+        title: 'Raghav Bhaiya',
+        price: 130,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 10,
+        image: 'assets/img/portfolio/threemukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 180,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 11,
+        image: 'assets/img/portfolio/fourmukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 12,
+        image: 'assets/img/portfolio/sphatikmala3.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 13,
+        image: 'assets/img/portfolio/sphatikmala3.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 14,
+        image: 'assets/img/portfolio/rudrakshmala6.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 15,
+        image: 'assets/img/portfolio/rudrakshmala8.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 16,
+        image: 'assets/img/portfolio/onemukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'beads',
+    },
+    {
+        id: 17,
+        image: 'assets/img/portfolio/twomukhi.jpg',
+        title: 'Raghav Bhaiya',
+        price: 130,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 18,
+        image: 'assets/img/portfolio/threemukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 180,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 19,
+        image: 'assets/img/portfolio/fourmukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 20,
+        image: 'assets/img/portfolio/sphatikmala3.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 21,
+        image: 'assets/img/portfolio/sphatikmala3.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 22,
+        image: 'assets/img/portfolio/rudrakshmala6.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 23,
+        image: 'assets/img/portfolio/rudrakshmala8.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 24,
+        image: 'assets/img/portfolio/onemukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'beads',
+    },
+    {
+        id: 25,
+        image: 'assets/img/portfolio/twomukhi.jpg',
+        title: 'Raghav Bhaiya',
+        price: 130,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 26,
+        image: 'assets/img/portfolio/threemukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 180,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 27,
+        image: 'assets/img/portfolio/fourmukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 28,
+        image: 'assets/img/portfolio/sphatikmala3.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 29,
+        image: 'assets/img/portfolio/sphatikmala3.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 30,
+        image: 'assets/img/portfolio/rudrakshmala6.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 31,
+        image: 'assets/img/portfolio/rudrakshmala8.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 32,
+        image: 'assets/img/portfolio/onemukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'beads',
+    },
+    {
+        id: 33,
+        image: 'assets/img/portfolio/twomukhi.jpg',
+        title: 'Raghav Bhaiya',
+        price: 130,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 34,
+        image: 'assets/img/portfolio/threemukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 180,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 35,
+        image: 'assets/img/portfolio/fourmukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 36,
+        image: 'assets/img/portfolio/sphatikmala3.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 37,
+        image: 'assets/img/portfolio/sphatikmala3.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 38,
+        image: 'assets/img/portfolio/rudrakshmala6.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 39,
+        image: 'assets/img/portfolio/rudrakshmala8.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 40,
+        image: 'assets/img/portfolio/onemukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'beads',
+    },
+    {
+        id: 41,
+        image: 'assets/img/portfolio/twomukhi.jpg',
+        title: 'Raghav Bhaiya',
+        price: 130,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 42,
+        image: 'assets/img/portfolio/threemukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 180,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 43,
+        image: 'assets/img/portfolio/fourmukhi.webp',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 44,
+        image: 'assets/img/portfolio/sphatikmala3.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 45,
+        image: 'assets/img/portfolio/sphatikmala3.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'sphatikmala',
+    },
+    {
+        id: 46,
+        image: 'assets/img/portfolio/rudrakshmala6.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'rudrakshmala',
+    },
+    {
+        id: 47,
+        image: 'assets/img/portfolio/rudrakshmala8.jpeg',
+        title: 'Raghav Bhaiya',
+        price: 120,
+        category: 'rudrakshmala',
+    },
 ];
+
 
 const categories = [...new Set(product.map((item) => { return item }))];
 let cart = [];
@@ -78,7 +358,7 @@ function filterProducts() {
 function displayProducts(products) {
     let i = 0;
     document.getElementById('root').innerHTML = products.map((item) => {
-        var { image, title, price } = item;
+        var { image, title, price,id } = item;
         return (
             `<div class='box'>
                 <div class='img-box'>
@@ -88,7 +368,7 @@ function displayProducts(products) {
                     <p>${title}</p>
                     <h2>$ ${price}.00</h2>` +
             `<button>
-                <a href="portfolio-details.html#rudrakashabeads-section" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details">
+                <a href="portfolio-details.html#${id}" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details">
                     Details
                 </a>
             </button>` +
